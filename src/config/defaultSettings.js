af075e209f83e5d127f17608bfe56ed99b45a1c1
caixiaomao <532001566@qq.com>
2020-10-15 15:53:23 +0800

    增加侧边栏中文说明、修改默认主题配置

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -23,7 +23,7 @@ export default {
   menu: {
     locale: false
   },
-  title: 'Ant Design Pro',
+  title: 'Cloud Miao',
   pwa: false,
   iconfontUrl: '',
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true'
--- src/locales/lang/zh-CN.js
+++ src/locales/lang/zh-CN.js
@@ -13,7 +13,27 @@ const locale = {
   'menu.dashboard': '仪表盘',
   'menu.dashboard.analysis': '分析页',
   'menu.dashboard.monitor': '监控页',
-  'menu.dashboard.workplace': '工作台'
+  'menu.dashboard.workplace': '工作台',
+
+  'app.setting.pagestyle': '整体风格设置',
+  'app.setting.pagestyle.light': '明亮主题',
+  'app.setting.pagestyle.dark': '黑暗主题',
+  'app.setting.pagestyle.realdark': '真正黑暗主题',
+  'app.setting.themecolor': '主题色',
+  'app.setting.navigationmode': '导航模式',
+  'app.setting.content-width': '内容区域宽度',
+  'app.setting.fixedheader': '固定头部',
+  'app.setting.fixedsidebar': '固定侧边栏',
+  'app.setting.sidemenu': '侧边栏菜单布局',
+  'app.setting.topmenu': '顶部菜单布局',
+  'app.setting.content-width.fixed': '固定',
+  'app.setting.content-width.fluid': '自适应',
+  'app.setting.othersettings': '其它设置',
+  'app.setting.weakmode': '色弱模式',
+  'app.setting.copy': '拷贝设置',
+  'app.setting.loading': '加载主题',
+  'app.setting.copyinfo': '拷贝成功，请替换 defaultSettings in src/models/setting.js',
+  'app.setting.production.hint': '设置面板仅在开发环境中展示，生产环境请手动修改配置'
 }
 
 export default {
